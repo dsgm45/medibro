28b4d2d96e1da776b9e3641a8708bac408870086
Dpk <dpk@Dpks-MacBook-Air.local>
2026-08-13 22:25:07 +0530

    Backlog batch 1: remove placeholder phone number, add Go Back link to legal pages

--- tests/test_legal_pages.py
+++ tests/test_legal_pages.py
@@ -19,6 +19,21 @@ class TestLegalPagesAccessibleWithoutLogin:
         assert b'Medical Disclaimer' in resp.data
 
 
+class TestGoBackLink:
+    def test_terms_has_go_back_link(self, client):
+        resp = client.get('/legal/terms')
+        assert b'Go Back' in resp.data
+        assert b'history.back()' in resp.data
+
+    def test_privacy_has_go_back_link(self, client):
+        resp = client.get('/legal/privacy')
+        assert b'Go Back' in resp.data
+
+    def test_disclaimer_has_go_back_link(self, client):
+        resp = client.get('/legal/disclaimer')
+        assert b'Go Back' in resp.data
+
+
 class TestLegalPageContent:
     def test_terms_covers_no_default_doctor_patient_relationship(self, client):
         resp = client.get('/legal/terms')
@@ -73,3 +88,7 @@ class TestFooterLinks:
         resp = client.get('/')
         assert b'Privacy &amp; HIPAA' not in resp.data
         assert b'Privacy & HIPAA' not in resp.data
+
+    def test_landing_page_has_no_placeholder_phone_number(self, client):
+        resp = client.get('/')
+        assert b'555-0199' not in resp.data
